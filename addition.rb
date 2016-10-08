8f8189a3e5d2b07b03c254adbeeeeec16582ce82
dapeng126 <zhangdapeng89@126.com>
2016-10-08 23:02:03 +0800

    添加 code

---
+++ addition.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+puts 6 + 11
+puts 20 - 11
+puts 8 * 10
+puts 100 / 10
+puts 10 ** 4
+puts 15 % 4
---
+++ ifcondition.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+your_age = 19
+
+if your_age <= 17
+  puts "You are not old enough for this site!"
+elsif your_age > 18
+  puts "Go to site: http://snakeio.com"
+else
+  puts "what is your gender?"
+end
