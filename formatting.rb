dace94bb34d75476e3ddd80610d6b4b954e767c8
dapeng126 <zhangdapeng89@126.com>
2016-10-09 09:18:30 +0800

    add formatting

---
+++ formatting.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+my_name = "zhangdapeng"
+
+puts my_name.capitalize
+puts my_name.upcase
+puts my_name.downcase
+puts my_name.reverse
+puts my_name.length
+
+star_war_seven = "Jar Jar Binks Han Solo Luke lens flare"
+puts star_war_seven[14..26]
+puts star_war_seven[15]
+$end
