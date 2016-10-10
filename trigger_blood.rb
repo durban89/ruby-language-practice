6d4e1f541c36c067eb616b8c9ea0edd32cdc6450
dapeng126 <zhangdapeng89@126.com>
2016-10-10 09:32:39 +0800

    split and array 的使用

---
+++ trigger_blood.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+rock_room = "Mary Kate Olson Lindsay Lohan Charlie Sheen"
+puts rock_room.split
+
+date = "11/17/2016"
+puts date.split("/")
+
+puts date[2]
+puts date[0..5]
+puts date[0...5]
+
+array_families = ["Brady","Partridge","Bundy"]
+puts array_families
