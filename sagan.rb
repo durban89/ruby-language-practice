324c373256f8e4cb7249d54685e767fe580639d2
dapeng126 <zhangdapeng89@126.com>
2016-10-14 09:11:53 +0800

    add sagan func

---
+++ sagan.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+def sagan
+	puts "Somewhere something incredible is waiting to be known"
+end
+
+def osteen
+	puts "You can change your world by changeing your word."
+end
+
+osteen
+sagan
+puts "i like move it move it"
+
+osteen
+sagan
+puts "The watermelon crawl"
