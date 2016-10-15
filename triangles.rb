e0e0710c3c4595293a65230f01a39b812a840850
dapeng126 <zhangdapeng89@126.com>
2016-10-15 23:52:12 +0800

    add triangle

---
+++ triangles.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+print "What is triangles height?"
+h = gets.to_f
+print "What is triangles width?"
+w = gets.to_f
+area = 0.5 * h * w
+print "Guess what! Your triangles area is #{area}!"
