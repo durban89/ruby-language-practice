50b8b067f374ef88e699d9c5969b6d04c2e3ce7d
dapeng126 <zhangdapeng89@126.com>
2016-10-27 23:59:34 +0800

    udpate hash

--- hash_ages.rb
+++ hash_ages.rb
@@ -21,8 +21,12 @@ puts hash1
 puts hash1[:Jackson]
 
 hash2 = {
-	Jackson:=>12,
-	Sarah:=>12,
-	Johnson:=>30,
-	Mike:=>90
+	Jackson:12,
+	Sarah:12,
+	Johnson:30,
+	Mike:90
 }
+
+old_enough = hash2.select {|k,v| v > 21}
+puts old_enough 
+hash2.each_key {|key| puts key}
