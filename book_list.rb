cdd3fc3e7bce8bf2a898b2d5d23613705b00388e
dapeng126 <zhangdapeng89@126.com>
2016-10-29 10:59:26 +0800

    add book while

--- book_list.rb
+++ book_list.rb
@@ -6,22 +6,25 @@ novel = {
 	Way_of_Kings:6,
 }
 
-puts "Would you like add another book to youy hash? (Type yes or no)"
+answer = "yes"
+while answer == "yes"
+	puts "Would you like add another book to youy hash? (Type yes or no)"
 
-answer = gets.chomp
+	answer = gets.chomp
 
-case answer
-	when "yes"
-		puts "What book would you like to add?"
-		book = gets.chomp
-		novel[book]
-		puts "What rating from one to ten would you give this book?"
-		rating = gets.chomp
-		novel[book] = rating
-		puts "#{book} was been added to your hash with a rating of #{rating} "
+	case answer
+		when "yes"
+			puts "What book would you like to add?"
+			book = gets.chomp
+			novel[book.to_sym]
+			puts "What rating from one to ten would you give this book?"
+			rating = gets.chomp
+			novel[book.to_sym] = rating.to_i
+			puts "#{book} was been added to your hash with a rating of #{rating} "
 
-	when "no"
-		puts "Sounds good to me!"
+		when "no"
+			puts "Sounds good to me!"
+	end
 end
 
 puts "Here's what's in your hash now: #{novel}"
