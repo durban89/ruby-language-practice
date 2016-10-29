a79447b392ed6dfaf873f78091865915d620e595
dapeng126 <zhangdapeng89@126.com>
2016-10-29 23:51:33 +0800

    add crud

---
+++ crud.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+books = {
+	Wheel_of_Time:6,
+	Misborn:6,
+	Alchemist:7
+}
+
+puts "What would you like to do? Options [add,update,delete,list]"
+
+choice = gets.chomp
+case choice
+	when "add"
+		puts "What book would you liek to add to your hash?"
+		title = gets.chomp
+		if books[title.to_sym].nil?
+			puts "What rating do you give this book?(Bwtween 1 to 10)"
+			rating = gets.chomp
+			books[title.to_sym] = rating.to_i
+			puts "#{title} has been added to your hash with a rating of #{rating}"
+		else 
+			puts "That book is already in your hash"
+		end
+	
+
+	when "update"
+		puts "What book's rating would you like to change?"
+		title = gets.chomp
+		if books[title.to_sym].nil?
+			puts "Error! Books not found!"
+		else 
+			puts "What new rating would you give it?"
+			rating = gets.chomp
+			books[title.to_sym] = rating.to_i
+			puts "Your rating of #{title} has benn updated to #{rating}"
+		end
+	
+	puts books
+	
+	end
