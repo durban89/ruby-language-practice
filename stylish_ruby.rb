a8737bc3cf200e397fed0584dccf02e44be498a9
dapeng126 <zhangdapeng89@126.com>
2016-11-24 10:00:09 +0800

    添加 stylesh ruby ruby样式

---
+++ stylish_ruby.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# 命名一个文件
+this_is_a_file.rb
+
+# 声明变量
+forty_two = 42
+
+# 定义一个方法
+def  this_is_a_method
+  # do stuff
+end
+
+# 声明常量
+FOUR = 'four'
+FIVE = 5
+
+# 多行代码
+[1,2,3].each do |i|
+  # do stuff
+end
+
+# 单行代码
+[1,2,3].each { |i| # do sduf }
+
+# 类命名
+class MyfirstClass
+end
+
+class MysecondClass
+end
