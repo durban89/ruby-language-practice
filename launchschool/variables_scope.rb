2c3d0da376be92676e7cbc02d4f5685e2f17bca5
dapeng126 <zhangdapeng89@126.com>
2016-11-26 22:16:27 +0800

    variable scope exercises

---
+++ launchschool/variables_scope.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+#
+# a = 5
+
+# 3.times do |n|
+#   a = 3
+#   b = 5
+# end
+
+# puts a 
+# puts b
+
+# a = 5
+
+# def some_method
+#   a = 3
+# end
+
+# puts a
+# 
+
+arr = [1, 2, 3]
+
+for i in arr do
+  a = 5      # a is initialized here
+end
+
+puts a       # is it accessible here?
