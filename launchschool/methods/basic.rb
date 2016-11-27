fbb47b020d275844ba79785feecec76eb53a9829
dapeng126 <zhangdapeng89@126.com>
2016-11-27 21:10:08 +0800

    添加 method basic

---
+++ launchschool/methods/basic.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+# basic method
+def greeting(name)
+  "Hello, #{name}. How old are you?"
+end
+
+puts greeting('durban')
+
+x = 2
+
+puts x = 2
+
+p name="durban"
+
+four = "four"
+
+print something = "nothing"
+
+puts 
+
+# muliply method
+def multiply(num1, num2)
+  num1 * num2
+end
+puts multiply(4, 2)
+
+# nothing output
+# def scream(words)
+#   words = words + "!!!!!"
+#   return 
+#   puts words
+# end
+# scream("Yeap")
+# 
+# nothing return method
+def scream(words)
+  words = words + "!!!!!"
+  puts words
+end
+scream("Yeap")
