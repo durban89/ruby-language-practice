6f1fc496e170fa0ef3d0efaad5fb3aa2fe50c7b4
dapeng126 <zhangdapeng89@126.com>
2016-12-03 22:02:15 +0800

    modify countdown.rb

--- launchschool/loop_iteraters/countdown.rb
+++ launchschool/loop_iteraters/countdown.rb
@@ -9,3 +9,20 @@ while x >= 0
 end
 
 puts "Done !"
+
+
+x = gets.chomp.to_i
+while x >= 0
+  puts x
+  x -= 1
+end
+
+puts "Done!"
+
+x = gets.chomp.to_i
+until x < 0
+  puts x
+  x -= 1
+end
+
+puts "Done !"
