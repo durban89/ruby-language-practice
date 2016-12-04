9494dbde85c4b716ec769f355d3d883120ebcd5a
dapeng126 <zhangdapeng89@126.com>
2016-12-04 22:27:48 +0800

    modify countdown.rb

--- launchschool/loop_iteraters/countdown.rb
+++ launchschool/loop_iteraters/countdown.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # countdown.rb
 
+# example 1
 x = gets.chomp.to_i
 
 while x >= 0
@@ -10,7 +11,7 @@ end
 
 puts "Done !"
 
-
+# example 2
 x = gets.chomp.to_i
 while x >= 0
   puts x
@@ -19,6 +20,7 @@ end
 
 puts "Done!"
 
+# example 3
 x = gets.chomp.to_i
 until x < 0
   puts x
@@ -26,3 +28,17 @@ until x < 0
 end
 
 puts "Done !"
+
+
+# example 4
+x = gets.chomp.to_i
+for i in 1..x do
+  puts i
+end
+puts "Done !"
+
+x = [1,2,3,4,5]
+for i in x do
+  puts i
+end
+puts "Done!"
