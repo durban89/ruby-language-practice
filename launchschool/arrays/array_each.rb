b507bbad0c908c7e69588f8b40a70943ea774e61
dapeng126 <zhangdapeng89@126.com>
2016-12-05 23:50:57 +0800

    add array_each.rb

---
+++ launchschool/arrays/array_each.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+arr = [1,3,5,7,9,11]
+number = 3
+
+arr.each do |num|
+  if number == num
+    puts "#{number} is in the array"
+  end
+end
+
+if arr.include?(number)
+  puts "#{number} is in the array"
+end
