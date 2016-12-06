8d6c6d577fc0f9ad258b9ca39a49943c97d40fe3
dapeng126 <zhangdapeng89@126.com>
2016-12-06 23:21:54 +0800

    add array_index.rb

---
+++ launchschool/arrays/array_index.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# array_index.rb
+arr = [15,7,18,5,12,8,5,1]
+
+arr = arr.index(5)
+puts arr
+
+# arr = arr.index[5]
+# puts arr
+
+# puts d = arr[0]
+
+string = "Welcome to America!"
+puts a = string[6]
+puts b = string[11]
+puts c = string[19]
