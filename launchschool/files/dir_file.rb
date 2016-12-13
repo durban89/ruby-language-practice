d7dcefb361de4e6edc6146c689381bed9228cb17
dapeng126 <zhangdapeng89@126.com>
2016-12-13 22:31:14 +0800

    add dir_file.rb

---
+++ launchschool/files/dir_file.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env/ruby
+
+require "pathname"
+
+d = Dir.new(".")
+while file = d.read do
+  puts "#{file} has extension .txt " if File.extname(file) == '.txt'
+end
+
+pn = Pathname.new(".")
+pn.entries.each { |f| puts "#{f} has extension .txt" if f.extname == '.txt'}
