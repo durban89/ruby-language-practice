ba8870df822fb50be9f12c36b2f31821ddf33fd5
dapeng126 <zhangdapeng89@126.com>
2016-12-17 22:23:38 +0800

    add launchschool/regex.rb

---
+++ launchschool/regex.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+def has_a_b(string)
+  if string =~ /b/
+    puts "We have a match !"
+  else
+    puts "no match!"
+  end
+end
+   
+has_a_b("basketball")
+has_a_b("football")
+has_a_b("hockey")
+has_a_b("golf")
