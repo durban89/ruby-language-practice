a5ab612725859faa3ebb966bf818fcb711c5e50a
dapeng126 <zhangdapeng89@126.com>
2016-12-20 22:58:41 +0800

    add divide.rb

---
+++ launchschool/divide.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+def divide(number, divisor)
+  begin
+    answer = number / divisor
+  rescue ZeroDivisionError => e
+    puts e.message
+  end
+end
+
+puts divide(16, 4)
+puts divide(4,0)
+puts divide(14,7)
