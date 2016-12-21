c16afa306b78cfb673fd7ca8ae789d463bc0e57a
dapeng126 <zhangdapeng89@126.com>
2016-12-21 22:59:31 +0800

    add regex_check_in.rb

---
+++ launchschool/regex_check_in.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+def check_in(word)
+  if /lab/ =~ word
+    puts word
+  else
+    puts "No match"
+  end
+end
+
+
+check_in("laboratory")
+check_in("experiment")
+check_in("Pans Labyrinth")
+check_in("elaborate")
+check_in("polar bear")
