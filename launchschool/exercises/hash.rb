1e18bc76737d519ea83f63c552c49f12164e0579
dapeng126 <zhangdapeng89@126.com>
2016-12-25 12:02:30 +0800

    update hash.rb

--- launchschool/exercises/hash.rb
+++ launchschool/exercises/hash.rb
@@ -55,7 +55,7 @@ puts contacts
 puts "Joe's email is #{contacts["Joe Smith"][:email]}"
 puts "Sally's phone is #{contacts["Sally Johnson"][:phone]}"
 
-# 
+# entry
 contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
 contacts = {"Joe Smith" => {}}
 fields = [:email, :address, :phone]
@@ -65,3 +65,17 @@ contacts.each do |name, hash|
   end
 end
 puts contacts
+
+# multiple entries
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
+contacts.each_with_index do |(name, hash), index|
+  fields.each do |field|
+    hash[field] = contact_data[index].shift
+  end
+end
+puts contacts
+
