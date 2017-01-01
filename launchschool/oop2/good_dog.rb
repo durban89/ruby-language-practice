0c24e65df47db072d08cb2659e00c78108eba878
dapeng126 <zhangdapeng89@126.com>
2017-01-01 12:32:17 +0800

    添加 constant

--- launchschool/oop2/good_dog.rb
+++ launchschool/oop2/good_dog.rb
@@ -1,8 +1,16 @@
 class GoodDog
+  DOG_YEARs = 7
+
+  attr_accessor :name, :age
+
   @@number_of_dogs = 0
 
-  def initialize
+  def initialize(n,a)
     @@number_of_dogs += 1
+
+    self.name = n;
+    self.age = a * DOG_YEARs;
+
   end
 
   def self.total_number_of_dogs
@@ -16,10 +24,13 @@ end
 
 puts GoodDog.total_number_of_dogs
 
-dog1 = GoodDog.new
-dog2 = GoodDog.new
+dog1 = GoodDog.new('Bod', 12)
+dog2 = GoodDog.new('Nace', 11)
 
 puts GoodDog.total_number_of_dogs
+
+puts dog1.age
+puts dog2.age
 
 puts GoodDog.what_am_i
 
